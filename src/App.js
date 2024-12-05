c9686d19271c937cabcef0f3a9d5e938c36b8d34
Dellon <jy.dellon@gmail.com>
2024-12-05 14:53:23 +0100

    deploy

--- src/App.js
+++ src/App.js
@@ -114,14 +114,14 @@ function App() {
       <Routes>
       
         <Route path="/" element={<Accueil />} />
-        <Route path="/nos-services" element={<NosServices />} />
-        <Route path="/rgpd" element={<Rgpd />} />
-        <Route path="/etapesCreationSite" element={<EtapesDansLaCreationDunSite />} />
-        <Route path="/a-propos" element={<APropos />} />
-        <Route path="/portfolio" element={<Portfolio />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/formulaire-devis" element={<Formulaire />} />
-        <Route path="/devis" element={<Devis />} />
+        <Route path="/test/nos-services" element={<NosServices />} />
+        <Route path="/test/rgpd" element={<Rgpd />} />
+        <Route path="/test/etapesCreationSite" element={<EtapesDansLaCreationDunSite />} />
+        <Route path="/test/a-propos" element={<APropos />} />
+        <Route path="test//portfolio" element={<Portfolio />} />
+        <Route path="test//contact" element={<Contact />} />
+        <Route path="test//formulaire-devis" element={<Formulaire />} />
+        <Route path="test//devis" element={<Devis />} />
         </Routes>
 
       <Footer />
