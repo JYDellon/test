8f4e38538380ccdeae9e285156066de551a5fc37
Dellon <jy.dellon@gmail.com>
2024-12-10 17:35:00 +0100

    deploy

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,54 +1,129 @@
+// import React from 'react';
+// import JRAUTO from '../assets/images/PORTFOLIO.jpg';
+// import SECOND_SITE from '../assets/images/hebergement-deploiement.jpg'; 
+// import './Portfolio.css';
+
+// function Portfolio() {
+//     return (
+//         <div className="portfolio-container">
+//             <div className="portfolio-items">
+//                 <section className="portfolio-item">
+//                     <div className="portfolio-content">
+//                         <div className="portfolio-text">
+//                             <h3 className="portfolio-titre">
+//                                 JR Auto
+//                             </h3>
+//                             <div className="portfolio-description">
+                               
+//                                 JR Auto nous a sollicités pour réaliser leur site web afin de soutenir le démarrage de leur activité.
+                                
+//                             </div>
+//                             <div className="portfolio-description">
+                               
+//                                 Le défi était de créer un site vitrine en seulement une semaine. Nous avons redéfini le design du site avec une expérience utilisateur UX/UI de qualité...
+                               
+//                             </div>
+                            
+                            
+//                             <div className="portfolio-cta-container">
+//                                 <a href="https://jydellon.github.io/JRAuto/" target="_blank" rel="noopener noreferrer" className="portfolio-cta">
+//                                     Visiter le site
+//                                 </a>
+//                             </div>
+//                         </div>
+//                         <div className='imagecontainer'>
+//                             <a href="https://jydellon.github.io/JRAuto/" target="_blank" rel="noopener noreferrer">
+//                                 <img src={JRAUTO} alt="Aperçu du site JR Auto" className="portfolio-image" />
+//                             </a>
+//                         </div>
+//                     </div>
+//                 </section>
+//                 <section className="portfolio-item">
+//                     <div className="portfolio-content">
+//                         <div className="portfolio-text">
+//                             <h1 className="portfolio-titre">
+//                                 Ashanti beauty
+//                             </h1>
+//                             <p className="portfolio-description">
+//                                 Ashanti beauty nous a sollicités alors que j'étais en formation pour réaliser un site web afin de soutenir le démarrage de son activité...
+//                             </p>
+//                             <h3 className="portfolio-solution-titre">Solution mise en place</h3>
+//                             <p className="portfolio-solution">
+//                                 Le défi était de créer un site vitrine en seulement une semaine. Nous avons redéfini le design du site avec une expérience utilisateur UX/UI de qualité...
+//                             </p>
+//                             <div className="portfolio-cta-container">
+//                                 <a href="https://jydellon.github.io/JRAuto/" target="_blank" rel="noopener noreferrer" className="portfolio-cta">
+//                                     Visiter le site
+//                                 </a>
+//                             </div>
+//                         </div>
+//                         <div className='imagecontainer'>
+//                             <a href="https://jydellon.github.io/JRAuto/" target="_blank" rel="noopener noreferrer">
+//                                 <img src={SECOND_SITE} alt="Aperçu du site JR Auto" className="portfolio-image" />
+//                             </a>
+//                         </div>
+                        
+//                     </div>
+//                 </section>
+//             </div>
+//         </div>
+//     );
+// }
+
+// export default Portfolio;
+
+
+
+
+
+
+
+
+
+
 import React from 'react';
 import JRAUTO from '../assets/images/PORTFOLIO.jpg';
-import SECOND_SITE from '../assets/images/hebergement-deploiement.jpg'; 
+import SECOND_SITE from '../assets/images/hebergement-deploiement.jpg';
 import './Portfolio.css';
 
 function Portfolio() {
     return (
         <div className="portfolio-container">
             <div className="portfolio-items">
+                {/* Étude de cas 1 */}
                 <section className="portfolio-item">
                     <div className="portfolio-content">
                         <div className="portfolio-text">
-                            <h3 className="portfolio-titre">
-                                JR Auto
-                            </h3>
-                            <div className="portfolio-description">
-                               
+                            <h3 className="portfolio-titre">JR Auto</h3>
+                            <p className="portfolio-description">
                                 JR Auto nous a sollicités pour réaliser leur site web afin de soutenir le démarrage de leur activité.
-                                
-                            </div>
-                            <div className="portfolio-description">
-                               
+                            </p>
+                            <p className="portfolio-description">
                                 Le défi était de créer un site vitrine en seulement une semaine. Nous avons redéfini le design du site avec une expérience utilisateur UX/UI de qualité...
-                               
-                            </div>
-                            
-                            
+                            </p>
                             <div className="portfolio-cta-container">
                                 <a href="https://jydellon.github.io/JRAuto/" target="_blank" rel="noopener noreferrer" className="portfolio-cta">
                                     Visiter le site
                                 </a>
                             </div>
                         </div>
-                        <div className='imagecontainer'>
+                        <div className="image-container">
                             <a href="https://jydellon.github.io/JRAuto/" target="_blank" rel="noopener noreferrer">
                                 <img src={JRAUTO} alt="Aperçu du site JR Auto" className="portfolio-image" />
                             </a>
                         </div>
                     </div>
                 </section>
+
+                {/* Étude de cas 2 */}
                 <section className="portfolio-item">
                     <div className="portfolio-content">
                         <div className="portfolio-text">
-                            <h1 className="portfolio-titre">
-                                Ashanti beauty
-                            </h1>
+                            <h3 className="portfolio-titre">Ashanti Beauty</h3>
                             <p className="portfolio-description">
-                                Ashanti beauty nous a sollicités alors que j'étais en formation pour réaliser un site web afin de soutenir le démarrage de son activité...
+                                Ashanti Beauty nous a sollicités alors que j'étais en formation pour réaliser un site web afin de soutenir le démarrage de son activité...
                             </p>
-                            <h3 className="portfolio-solution-titre">Solution mise en place</h3>
-                            <p className="portfolio-solution">
+                            <p className="portfolio-description">
                                 Le défi était de créer un site vitrine en seulement une semaine. Nous avons redéfini le design du site avec une expérience utilisateur UX/UI de qualité...
                             </p>
                             <div className="portfolio-cta-container">
@@ -57,12 +132,11 @@ function Portfolio() {
                                 </a>
                             </div>
                         </div>
-                        <div className='imagecontainer'>
+                        <div className="image-container">
                             <a href="https://jydellon.github.io/JRAuto/" target="_blank" rel="noopener noreferrer">
-                                <img src={SECOND_SITE} alt="Aperçu du site JR Auto" className="portfolio-image" />
+                                <img src={SECOND_SITE} alt="Aperçu du site Ashanti Beauty" className="portfolio-image" />
                             </a>
                         </div>
-                        
                     </div>
                 </section>
             </div>
@@ -71,13 +145,3 @@ function Portfolio() {
 }
 
 export default Portfolio;
-
-
-
-
-
-
-
-
-
-
